8b21bff605fca3639cf468f1afdaf90d3387460b
Justin Murdock <justin.murdock96@gmail.com>
2015-10-26 21:09:46 -0700

    single company

--- single_company.php
+++ single_company.php
@@ -1,7 +1,21 @@
 <?php
 include('images/CURLQuery.php');
-$name = $_REQUEST['name'];
 $id = $_REQUEST['id'];
-get_company_image_from_string($name, $id);
+
+$csv_data = array_map('str_getcsv', file('top_1000_industries.csv'));
+
+foreach($csv_data as $key => $datum){
+    if($key < $id) {
+        echo $key;
+        continue; // start from 352
+    }
+    echo "<br>" .$key . ":" . $datum[0] . "\n<br>";
+    sleep(rand(5, 15));
+    if($_REQUEST['extra']){
+        $datum[0] .= " {$_REQUEST['extra']}";
+    }
+    get_company_image_from_string($datum[0], $key);
+    exit();
+}
 
 ?>
