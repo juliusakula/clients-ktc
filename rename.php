10a7ce2d21fa969b6fa2f5397d750b15afd6a674
Justin Murdock <justin.murdock96@gmail.com>
2015-11-04 23:39:26 -0500

    modified output

--- rename.php
+++ rename.php
@@ -1,35 +1,25 @@
 <?php
 
-    $csv_data = array_map('str_getcsv', file('top_1000_industries.csv'));
+$csv_data = array_map('str_getcsv', file('top_1000_industries.csv'));
 
-    //replace all whitespaces with -
-    function replace_white($name) {
-        $name = preg_replace("/[\s]/", "-", $name);
-        return $name;
-    }
-
+//replace all whitespaces with -
+function replace_white($name) {
+    $name = preg_replace("/[\s]/", "-", $name);
+    return $name;
+}
 
 foreach($csv_data as $key => $datum){
-    $name=$datum[0];
-    echo "$key";
-    echo "$row_name";
-
-
-
-    $name=replace_white($name);
+    $name=replace_white($datum[0]);
 
     $src= "cleaned_1000/{$key}.png";
     $dest="images/{$name}.png";
-
-
     $cp_string="cp {$src} {$dest}";
 
-    exec($cp_string);
-
-    echo $cp_string;
-
-    echo $key . ":" . $datum[0] . "\n";
-
+    #exec($cp_string);
+    #echo $cp_string;
+    echo "<div style=\"background-color: #dfdfdf\">";
+    echo "<h1>{$name} <i><small>row#{$key}</small></i></h1> <img src=\"{$dest}\">";
+    echo "</div>";
+    
 }
-
 ?>
