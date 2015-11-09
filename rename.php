fe189cc229b6702eff98963a97b802b0d4f0b6b0
Devon Warmack <devonwarmack@karmaconsulting.com>
2015-11-08 22:47:47 -0600

    started on src change for client

--- rename.php
+++ rename.php
@@ -1,6 +1,6 @@
 <?php
 
-$csv_data = array_map('str_getcsv', file('C:/git-lyris/parse-data-dot-com/top_1000_industries.csv'));
+$csv_data = array_map('str_getcsv', file('/Users/devon/Documents/clients-ktc/top_1000_industries.csv'));
 
 //replace all whitespaces with -
 function replace_white($name) {
@@ -9,18 +9,21 @@ function replace_white($name) {
     return $name;
 }
 
+echo '<script type="text/javascript" src="jquery-1.11.3.min.js"></script>';
+echo '<link text="text/css" rel="stylesheet" href="styles.css">';
+
 foreach($csv_data as $key => $datum){
     $name=replace_white($datum[0]);
 
     $src= "C:/git-lyris/parse-data-dot-com/images/{$key}.png";
     $dest="C:/git-lyris/parse-data-dot-com/images/{$name}.png";
-    $dest_url="images/{$name}.png";
+    $dest_url="images/{$key}.png";
     $cp_string="cp {$src} {$dest}";
 
-    $result = exec($cp_string);
+    //$result = exec($cp_string);
     echo $cp_string . "\n<br>";
     echo $result;
-    echo "<div style=\"background-color: #dfdfdf\">";
+    echo "<div class=\"logo logo{$key}\">";
     echo "<h1>{$name} <i><small>row#{$key}</small></i></h1> <img src=\"{$dest_url}\">";
     echo "</div>";
     
---
+++ save_image.php
@@ -0,0 +1,12 @@
+<?php
+
+// query setup similar to: {"success":false,"message":"You did not set 'my_query' GET parameter."}
+// send a request similar to: {"success":true,"message":"this_is_my_request_parameter_value"}
+
+
+
+
+
+
+
+?>
